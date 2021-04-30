635046127a690345dc6ecd70785107d299538b14
Alexander Nguyen <cernvii@gmail.com>
2021-04-29 21:56:11 -0400

    server skeleton

---
+++ server.js
@@ -0,0 +1,34 @@
+// load .env data into process.env
+require('dotenv').config();
+
+// Web server config
+const PORT       = process.env.PORT || 8080;
+const ENV        = process.env.ENV || "development";
+const express    = require("express");
+const bodyParser = require("body-parser");
+const sass       = require("node-sass-middleware");
+const app        = express();
+const morgan     = require('morgan');
+
+app.use(morgan('dev'));
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
+// Home page
+// Warning: avoid creating more routes in this file!
+// Separate them into separate routes files (see above).
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
